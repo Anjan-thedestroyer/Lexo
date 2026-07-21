89c20ddfe83d3cd2eb59a716396de0d5fc9a1891
Anjan-thedestroyer <paudelabinash28@gmail.com>
2026-07-21 12:43:52 +0000

    IIdentityRegisterTest commit -1

--- Smart-Contracts/src/interfaces/IIdentityRegisterTest.sol
+++ Smart-Contracts/src/interfaces/IIdentityRegisterTest.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
-
+//@title IIdentityRegisterTest
+//@dev This interface defines the functions for the Identity Register Test contract
 interface IIdentityRegisterTest {
     function isVerified(address wallet) external view returns (bool);
     function getIdentity(bytes32 identityHash) external view returns (bool isVerifiedStatus, bool isRestrictedStatus, address root, address[] memory walletList);
